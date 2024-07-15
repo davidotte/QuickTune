ad5620405cbc819af016e7e86e307ee0a3d73753
David Otte <ottedavid01@gmail.com>
2024-07-15 17:52:52 +0200

    Remove unnecessary files and add relevant files from exam code template

--- examples/quicktuning.py
+++
@@ -1,8 +0,0 @@
-from qtt.factory import get_optimizer
-from qtt.tuners import QuickTuner
-from qtt.finetune.cv.classification import finetune_script
-
-optimizer = get_optimizer("mtlbm/micro")
-qt = QuickTuner(optimizer, finetune_script)
-task_info = {"data_path": "path/to/data"}
-qt.run(task_info=task_info, time_budget=3600)
